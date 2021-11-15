254c614dc7f971fdd8305fc6bf0da366d966a71d
Carter <ASHLEY.CARTER@LibertyMutual.com>
2021-11-15 15:04:36 -0500

    added services page and link functionality

---
+++ src/pages/services/Services.js
@@ -0,0 +1,15 @@
+import Header from "../../components/header/Header";
+import GridCards from "../../components/gridcards/GridCards";
+import Footer from "../../components/footer/Footer";
+
+function Services(props) {
+  return (
+    <div className="Services">
+        <Header/>
+        <GridCards/>
+        <Footer/>
+    </div>
+  );
+}
+
+export default Services;
